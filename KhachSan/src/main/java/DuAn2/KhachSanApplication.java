5e9a3fa2096f3050e30a0f870d6c9931e6dad0f1
MeatyTT <63178475+MeatyTT@users.noreply.github.com>
2022-06-01 20:56:34 +0700

    first commit

---
+++ KhachSan/src/main/java/DuAn2/KhachSanApplication.java
@@ -0,0 +1,13 @@
+package DuAn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhachSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhachSanApplication.class, args);
+	}
+
+}
---
+++ KhachSan/src/test/java/DuAn2/KhachSanApplicationTests.java
@@ -0,0 +1,13 @@
+package DuAn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhachSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
